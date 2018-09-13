8da552cfdd96e4a87c79d4c9c4287fcc675a2c23
adaao <armascarelli@gmail.com>
2018-09-13 19:13:10 -0300

    exercicio imc

--- exerciciosVim/pg54_2_33/Pg54_2_33.java
+++ exerciciosVim/pg54_2_33/Pg54_2_33.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.*;
+
 public class Pg54_2_33{
-   public static void main(String [] args){
-      
+   public static void main(String[] args){
+   	Scanner consoleInput = new Scanner(System.in);
+	int peso,
+	    altura,
+	    imc,
+	    alturaQ;
+
+	System.out.printf("CALCULO DE IMC%n");
+   	System.out.printf("Digite seu peso em Quilos: %n");
+	peso = consoleInput.nextInt();
+	System.out.printf("%nDigite sua altura: %n");
+	altura = consoleInput.nextInt();
+	alturaQ = altura * altura;
+	System.out.println("teste");
+	imc = peso / alturaQ;
+
+	System.out.printf("Seu imc é %d", imc);
    }
+
 }
